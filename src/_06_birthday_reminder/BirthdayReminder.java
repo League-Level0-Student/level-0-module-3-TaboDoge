56007abd86951ee40ea5bbb9dbe04d542c107844
League Student <league@iMac-23.attlocal.net>
2019-08-13 18:28:06 -0700

    :arrow_backward:

--- src/_06_birthday_reminder/BirthdayReminder.java
+++ src/_06_birthday_reminder/BirthdayReminder.java
@@ -11,21 +11,36 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
-
+		String momsBirthday = "June 13th";
+		String dadsBirthday = "August 8th";
+		String myBirthday = "March 14th";
+		String sisBirthday = "October 21st";
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+	String Birthday = JOptionPane.showInputDialog("Who's birthday do you want?(mom's, dad's, sister's, mine");
+	System.out.println(Birthday + ":");
 		// 3. Print out what the user typed
 		
 		// 4. if user asked for "mom"
 			//print mom's birthday
+	if(Birthday.equals("mom")) {
+		System.out.println(momsBirthday);
+	}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+	else if (Birthday.equals("dad")) {
+		System.out.println(dadsBirthday);
+		}
 		// 6. if user asked for your name
 			// print myBirthday
+	else if (Birthday.equals("yours")) {
+		System.out.println(myBirthday);
+		}
+	else if(Birthday.equals("sister")) {
+		System.out.println(sisBirthday);
+	}
 		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+	else {
+		System.out.println("Sorry, i don't remember that person's birthday");
+	}
 	} 
 }
---
+++ src/_07_years_alive/Years.java
@@ -0,0 +1,9 @@
+package _07_years_alive;
+
+public class Years {
+public static void main(String[] args) {
+	for (int i = 2006; i <2020; i++) {
+		System.out.println(i);
+	}
+}
+}
