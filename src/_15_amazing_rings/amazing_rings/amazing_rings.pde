cabfd7e18ad8aa2848f62500b512d394e79dc5fb
League Student <league@iMac-27.attlocal.net>
2019-09-08 12:59:09 -0700

    :100:

---
+++ src/_15_amazing_rings/amazing_rings/amazing_rings.pde
@@ -0,0 +1,48 @@
+void setup(){
+  int t = 800;
+  int o = 200;
+ size(1000, 400);
+    int f = 50;
+ int e =45;
+  for(int i = 40; i > 1; i--){
+  
+
+ 
+ if (i % 2 ==0){
+
+  
+  noFill();
+  ellipse(o ,200,i*10 , i*10);
+ }
+ 
+ else{
+  noFill();
+  ellipse(o, 200 , i*10,i*10 );
+ }
+  for(int eu = 40; eu > 1; eu--){
+
+    
+ for (int rr = 40; rr > 1; rr--){
+ if (i % 2 ==0){
+
+  
+  noFill();
+  ellipse(t,200,i*10, i*10);
+ }
+ 
+ else{
+  noFill();
+  ellipse(t, 200 , i*10,i*10 );
+ }
+ }s
+  
+ 
+
+  
+  }
+}
+
+}
+void draw(){
+
+}
--- src/_16_obedient_robot/obedientrobot.java
+++ src/_16_obedient_robot/obedientrobot.java
@@ -1,15 +1,47 @@
 package _16_obedient_robot;
 
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class obedientrobot {
+private static final int Color = 0;
 public static void main(String[] args) {
-	Robot hi = new Robot();
-	
-	
+	for (int i = 0; i < 100; i ++) {
+	String shape = JOptionPane.showInputDialog("What shape do you want: Square, Triangle or circle?");
+	//shapes
+	if (shape.equals("square")) {
+		drawSquare();
+	}
+	else if (shape.equals("triangle")) {
+		drawTriangle();
+	}
+	else{
+		drawCircle();
+	}
+	}
 }
-void drawSquare(){
+static void drawSquare(){
 	Robot square = new Robot();
+	String color = JOptionPane.showInputDialog("What color: red, blue or black?");
+	// colors section
+		Color red = new Color(255, 0, 0);
+		Color blue = new Color(20 , 0, 255);
+		Color black = new Color(0, 0, 5);
+		if (color.equals("red")) {
+			square.setPenColor(red);
+		}
+		else if (color.equals("blue")) {
+			square.setPenColor(blue);
+		}
+		else {
+			square.setPenColor(black);
+		}
+	square.setSpeed(100);
+	square.move(-300);
+	square.penDown();
 square.move(200);
 square.turn(90);
 square.move(200);
@@ -18,7 +50,65 @@ square.move(200);
 square.turn(90);
 square.move(200);
 square.turn(90);
+square.penUp();
+square.move(800);
+}
+static void drawTriangle() {
+	Robot triangle = new Robot();
+	String color = JOptionPane.showInputDialog("What color: red, blue or black?");
+	// colors section
+		Color red = new Color(255, 0, 0);
+		Color blue = new Color(20 , 0, 255);
+		Color black = new Color(0, 0, 5);
+		if (color.equals("red")) {
+			triangle.setPenColor(red);
+		}
+		else if (color.equals("blue")) {
+			triangle.setPenColor(blue);
+		}
+		else {
+			triangle.setPenColor(black);
+		}
+	triangle.setSpeed(100);
+	triangle.turn(270);
+	triangle.move(300);
+	triangle.penDown();
 
+	for(int i = 0; i < 3; i++) {
+	triangle.turn(120);
+	triangle.move(200);
+	}
+	triangle.penUp();
+	triangle.move(600);
+}
+static void drawCircle() {
+	Robot circle = new Robot();
+	circle.setSpeed(1000);
+	String color = JOptionPane.showInputDialog("What color: red, blue or black?");
+	// colors section
+		Color red = new Color(255, 0, 0);
+		Color blue = new Color(20 , 0, 255);
+		Color black = new Color(0, 0, 5);
+		if (color.equals("red")) {
+			circle.setPenColor(red);
+		}
+		else if (color.equals("blue")) {
+			circle.setPenColor(blue);
+		}
+		else {
+			circle.setPenColor(black);
+		}
+		
+	circle.move(90);
+	circle.penDown();
+	
+	for(int y = 0; y < 180; y++) {
+	circle.turn(2);
+	circle.move(3);
+	
+	}
+	circle.penUp();
+	circle.move(600);
+}
 }
 
-}
